579b334f1c0bc8fb96c669c9127d8e0b374842b9
Andrea Chovanova <andreaa.chovanova@gmail.com>
2018-12-11 09:02:26 +0100

    ...

--- src/main/java/lekce1/LIghtning.java
+++ src/main/java/lekce1/LIghtning.java
@@ -5,16 +5,16 @@ public class LIghtning {
 	public static void main(String[] args) {
 		double time = 5.2;
 		int speed = 330;
-		boolean iAmSage;
+		boolean iAmSafe;
 		
 		double distance = time * speed;
 		
 		if(distance > 1000) {
-			iAmSage = true;
-			System.out.println("iAmSage " + iAmSage + ". You can enjoy the storm. ");
+			iAmSafe = true;
+			System.out.println("iAmSage " + iAmSafe + ". You can enjoy the storm. ");
 		}if(distance <= 1000) {
-			iAmSage = false;
-			System.out.println("iAmSage " + iAmSage + ". You'd better run.");
+			iAmSafe = false;
+			System.out.println("iAmSage " + iAmSafe + ". You'd better run.");
 		}
 	}
 }
