8bff6b599c66c0fe3289b2d48e0cd31360634ae0
Andrea Chovanova <andreaa.chovanova@gmail.com>
2019-03-05 13:29:44 +0100

    trieda input, lekce 6

--- src/main/java/lekce6/Input.java
+++ src/main/java/lekce6/Input.java
@@ -1,14 +1,19 @@
 package lekce6;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Input {
 	
 	private Scanner scan;
 	
-	public String getAnswerFromThePlayer() {
+	public String getStringAnswerFromTheUser() {
 		scan = new Scanner(System.in);
 		String answer = scan.nextLine();
 		return answer;
+	}
+	
+	public char getCharAnswerFromTheUser() throws IOException {
+		return (char) System.in.read();	
 	}
 }
