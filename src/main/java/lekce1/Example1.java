4f1514af6aebdac0b864553c25b904a0d84205dd
Andrea <hadka.ch@gmail.com>
2019-05-10 09:29:54 +0200

    Small change to test the git commit

--- src/main/java/lekce1/Example1.java
+++ src/main/java/lekce1/Example1.java
@@ -5,7 +5,7 @@ public class Example1 {
 	public static void main(String[] args) {
 		/*toto je komentar, kompilátor jeho obsah ignoruje. Tento koemntár může
 		 * byť na viac riadkov*/
-//		toto je tiez komentar. na jeden riadok. A dá sa lahko odkomentovat a zakomentovat.
+//		toto je tiez komentar. na jeden riadok. A dá sa lahko odkomentovat a zakomentovat. Small change to test git commit
 		System.out.println("Hello world!");
 	}
 }
